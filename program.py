742f0458ad9f429143afead2e90849a343f8db77
Dominykas <110461307+Dronzillla@users.noreply.github.com>
2024-04-05 17:19:26 +0300

    Updated main game loop to seperate 3 main menu items

--- program.py
+++ program.py
@@ -22,133 +22,133 @@ class Program:
         while self.running:
             # Show main menu
             main_menu_choice = self.show_main_menu()
-
             # A menu for central bank operations
             if main_menu_choice == "1. Central bank operations":
-                while True:
-                    sub_menu_choice = self.show_central_bank_menu()
-
-                    if sub_menu_choice == "1. Populate database with new data":
-                        while True:
-                            sub_sub_menu_choice = self.show_central_bank_1_menu()
-                            if sub_sub_menu_choice == "1. Add random new banks":
-                                count = self.get_not_negative_integer()
-                                generate_new_banks(count)
-                                print(f"Adding {count} new bank(s) to a database. ")
-                                print("-" * 30)
-                            elif sub_sub_menu_choice == "2. Add random new persons":
-                                count = self.get_not_negative_integer()
-                                generate_new_persons(count)
-                                print(f"Adding {count} new person(s) to a database. ")
-                                print("-" * 30)
-                            elif sub_sub_menu_choice == "3. Add random new accounts":
-                                count = self.get_not_negative_integer()
-                                try:
-                                    generate_new_accounts(count)
-                                except (NoPersonsError, NoBanksError) as e:
-                                    print(f"{e.args[0]}")
-                                else:
-                                    print(
-                                        f"Adding {count} new account(s) to a database. "
-                                    )
-                                    print("-" * 30)
-                            elif sub_sub_menu_choice == "4. Back to menu":
-                                break
-                            # TODO: Implement Create new bank, person, account
-
-                    elif sub_menu_choice == "2. View database data":
-                        while True:
-                            sub_sub_menu_choice = self.show_central_bank_2_menu()
-                            if sub_sub_menu_choice == "1. View banks":
-                                show_banks()
-                            elif sub_sub_menu_choice == "2. View persons":
-                                show_persons()
-                            elif sub_sub_menu_choice == "3. View accounts":
-                                show_accounts()
-                            elif sub_sub_menu_choice == "4. Back to menu":
-                                break
-
-                    elif sub_menu_choice == "3. Back to main menu":
-                        break
-
+                self.handle_central_bank_operations()
             # A menu for personal bank operations
             elif main_menu_choice == "2. Personal bank operations":
-                while True:
-                    sub_menu_choice = self.show_personal_bank_menu()
-
-                    if sub_menu_choice == "1. Validate identity":
-                        personal_code = input("PERSONAL CODE: ")
-                        if not personal_code_exists(personal_code):
-                            print(
-                                "ERROR. Person with provided personal code does not exist. "
-                            )
-                            continue
-                        else:
-                            greet_person(personal_code)
-                            while True:
-                                sub_sub_menu_choice = self.show_personal_bank_1_menu()
-                                # TODO procceed with operations
-                                if sub_sub_menu_choice == "1. View accounts":
-                                    show_personal_accounts(personal_code)
-                                elif sub_sub_menu_choice == "2. Withdraw money":
-                                    # Get account id to withdraw money from and validate data type
-                                    try:
-                                        account_id = self.get_user_id()
-                                    except ValueError:
-                                        print("ERROR. Invalid data type. ")
-                                        continue
-
-                                    # Else get account from db and check if account id is assigned to a person
-                                    account = select_account(personal_code, account_id)
-                                    if account is None:
-                                        print(
-                                            f"ERROR. You don't have an account with account id: {account_id}. "
-                                        )
-                                    else:
-                                        # Get the amount to withdraw
-                                        amount = self.get_not_negative_amount()
-
-                                        # Withdraw money
-                                        if account.balance >= amount:
-                                            withdraw(account, amount)
-                                            print(
-                                                f"Withdrawing {amount} from account with account id: {account_id}. "
-                                            )
-                                        else:
-                                            print("ERROR. Balance is not enough. ")
-
-                                elif sub_sub_menu_choice == "3. Deposit money":
-                                    # Get account id to deposit money and validate data type
-                                    try:
-                                        account_id = self.get_user_id()
-                                    except ValueError:
-                                        print("ERROR. Invalid data type. ")
-                                        continue
-
-                                    # Else get account from db and check if account id is assigned to a person
-                                    account = select_account(personal_code, account_id)
-                                    if account is None:
-                                        print(
-                                            f"ERROR. You don't have an account with account id: {account_id}. "
-                                        )
-                                    else:
-                                        # Get the amount to deposit
-                                        amount = self.get_not_negative_amount()
-
-                                        # Deposit money
-                                        deposit(account, amount)
-                                        print(
-                                            f"Depositing {amount} to account with account id: {account_id}. "
-                                        )
-                                        print("-" * 30)
-                                elif sub_sub_menu_choice == "4. Back to menu":
-                                    break
-
-                    elif sub_menu_choice == "2. Back to main menu":
-                        break
-
+                self.handle_personal_bank_operations()
+            # Exit from program
             elif main_menu_choice == "3. Exit from program":
                 self.stop()
+
+    def handle_central_bank_operations(self) -> None:
+        while True:
+            sub_menu_choice = self.show_central_bank_menu()
+
+            if sub_menu_choice == "1. Populate database with new data":
+                while True:
+                    sub_sub_menu_choice = self.show_central_bank_1_menu()
+                    if sub_sub_menu_choice == "1. Add random new banks":
+                        count = self.get_not_negative_integer()
+                        generate_new_banks(count)
+                        print(f"Adding {count} new bank(s) to a database. ")
+                        print("-" * 30)
+                    elif sub_sub_menu_choice == "2. Add random new persons":
+                        count = self.get_not_negative_integer()
+                        generate_new_persons(count)
+                        print(f"Adding {count} new person(s) to a database. ")
+                        print("-" * 30)
+                    elif sub_sub_menu_choice == "3. Add random new accounts":
+                        count = self.get_not_negative_integer()
+                        try:
+                            generate_new_accounts(count)
+                        except (NoPersonsError, NoBanksError) as e:
+                            print(f"{e.args[0]}")
+                        else:
+                            print(f"Adding {count} new account(s) to a database. ")
+                            print("-" * 30)
+                    elif sub_sub_menu_choice == "4. Back to menu":
+                        break
+                    # TODO: Implement Create new bank, person, account
+
+            elif sub_menu_choice == "2. View database data":
+                while True:
+                    sub_sub_menu_choice = self.show_central_bank_2_menu()
+                    if sub_sub_menu_choice == "1. View banks":
+                        show_banks()
+                    elif sub_sub_menu_choice == "2. View persons":
+                        show_persons()
+                    elif sub_sub_menu_choice == "3. View accounts":
+                        show_accounts()
+                    elif sub_sub_menu_choice == "4. Back to menu":
+                        break
+
+            elif sub_menu_choice == "3. Back to main menu":
+                break
+
+    def handle_personal_bank_operations(self) -> None:
+        while True:
+            sub_menu_choice = self.show_personal_bank_menu()
+
+            if sub_menu_choice == "1. Validate identity":
+                personal_code = input("PERSONAL CODE: ")
+                if not personal_code_exists(personal_code):
+                    print("ERROR. Person with provided personal code does not exist. ")
+                    continue
+                else:
+                    greet_person(personal_code)
+                    while True:
+                        sub_sub_menu_choice = self.show_personal_bank_1_menu()
+                        # TODO procceed with operations
+                        if sub_sub_menu_choice == "1. View accounts":
+                            show_personal_accounts(personal_code)
+                        elif sub_sub_menu_choice == "2. Withdraw money":
+                            # Get account id to withdraw money from and validate data type
+                            try:
+                                account_id = self.get_user_id()
+                            except ValueError:
+                                print("ERROR. Invalid data type. ")
+                                continue
+
+                            # Else get account from db and check if account id is assigned to a person
+                            account = select_account(personal_code, account_id)
+                            if account is None:
+                                print(
+                                    f"ERROR. You don't have an account with account id: {account_id}. "
+                                )
+                            else:
+                                # Get the amount to withdraw
+                                amount = self.get_not_negative_amount()
+
+                                # Withdraw money
+                                if account.balance >= amount:
+                                    withdraw(account, amount)
+                                    print(
+                                        f"Withdrawing {amount} from account with account id: {account_id}. "
+                                    )
+                                else:
+                                    print("ERROR. Balance is not enough. ")
+
+                        elif sub_sub_menu_choice == "3. Deposit money":
+                            # Get account id to deposit money and validate data type
+                            try:
+                                account_id = self.get_user_id()
+                            except ValueError:
+                                print("ERROR. Invalid data type. ")
+                                continue
+
+                            # Else get account from db and check if account id is assigned to a person
+                            account = select_account(personal_code, account_id)
+                            if account is None:
+                                print(
+                                    f"ERROR. You don't have an account with account id: {account_id}. "
+                                )
+                            else:
+                                # Get the amount to deposit
+                                amount = self.get_not_negative_amount()
+
+                                # Deposit money
+                                deposit(account, amount)
+                                print(
+                                    f"Depositing {amount} to account with account id: {account_id}. "
+                                )
+                                print("-" * 30)
+                        elif sub_sub_menu_choice == "4. Back to menu":
+                            break
+
+            elif sub_menu_choice == "2. Back to main menu":
+                break
 
     def stop(self):
         self.running = False
